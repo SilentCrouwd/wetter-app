2cdf2501107fa8302ea71060c46cc9190dd90672
SilentCrouwd <chris.buchholz.lauenau@googlemail.com>
2026-06-24 16:03:50 +0200

    refactor: extract API key and root URL into constants for better maintainability

--- src/api.js
+++ src/api.js
@@ -1,9 +1,11 @@
 const LOCAL_STORAGE_KEY = "CityArray";
+const API_KEY = "key=86a979bdd40a47c6b58140058260806";
+const API_ROOT = "https://api.weatherapi.com/v1";
 
 export async function getWetherData(id, days = 3) {
   try {
     const response = await fetch(
-      `https://api.weatherapi.com/v1/forecast.json?key=86a979bdd40a47c6b58140058260806&q=id:${id}&days=${days}&lang=de`,
+      `${API_ROOT}/forecast.json?${API_KEY}&q=id:${id}&days=${days}&lang=de`,
     );
     if (!response.ok) throw new Error("Netzwerk-Antwort war nicht ok");
     const WetherDataCurrent = await response.json();
@@ -26,9 +28,7 @@ export function setLocalStorage(insertedArray) {
 }
 
 export async function serchApi(input) {
-  const response = await fetch(
-    `https://api.weatherapi.com/v1/fsearch.json?key=86a979bdd40a47c6b58140058260806&q=${input}`,
-  );
+  const response = await fetch(`${API_ROOT}/search.json?${API_KEY}&q=${input}`);
   const searchCity = await response.json();
 
   return searchCity;
